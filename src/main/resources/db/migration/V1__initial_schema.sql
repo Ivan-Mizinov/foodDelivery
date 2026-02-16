b3ef0a1901c8e2b8767ac21e2b69c06dc0a527a0
Ivan Mizinov <mizinov.ivan@yandex.ru>
2026-02-16 10:59:29 +0300

    added flyway

---
+++ src/main/resources/db/migration/V1__initial_schema.sql
@@ -0,0 +1,50 @@
+CREATE TABLE IF NOT EXISTS users (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    address VARCHAR(255),
+    phone VARCHAR(255),
+    telegram VARCHAR(255),
+    created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITHOUT TIME ZONE
+);
+
+CREATE TABLE IF NOT EXISTS  menu_items (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    menu_category VARCHAR(255) NOT NULL,
+    price NUMERIC(10, 2) NOT NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITHOUT TIME ZONE
+);
+
+CREATE TABLE IF NOT EXISTS  orders (
+    id BIGSERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    total_price NUMERIC(10, 2) NOT NULL,
+    order_date TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITHOUT TIME ZONE,
+    CONSTRAINT fk32ql8ubntj5uh44ph9659tiih FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS  orders_menu_items (
+    order_id BIGINT NOT NULL,
+    menu_item_id BIGINT NOT NULL,
+    CONSTRAINT fkdi6e3l6rwcflf0x2r43aiux24 FOREIGN KEY (order_id) REFERENCES orders(id),
+    CONSTRAINT fksnnhhjghd1at8679p3ftedvnp FOREIGN KEY (menu_item_id) REFERENCES menu_items(id)
+);
+
+CREATE TABLE IF NOT EXISTS  deliveries (
+    id BIGSERIAL PRIMARY KEY,
+    order_id BIGINT NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    phone VARCHAR(255) NOT NULL,
+    delivery_time TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITHOUT TIME ZONE,
+    CONSTRAINT deliveries_order_id_key UNIQUE (order_id),
+    CONSTRAINT fk7isx0rnbgqr1dcofd5putl6jw FOREIGN KEY (order_id) REFERENCES orders(id)
+);
